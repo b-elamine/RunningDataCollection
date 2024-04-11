f614783f32f5041aef2bc9ad4d699e1575951688
belamine <m.bechorfa@esi-sba.dz>
2024-04-11 15:07:43 +0200

    Upgrade user experience

--- app/src/main/java/com/example/runnersDataCollection/MainActivity.java
+++ app/src/main/java/com/example/runnersDataCollection/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -32,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         done = (Button) findViewById(R.id.btnDone);
         next = (Button) findViewById(R.id.btnNext);
